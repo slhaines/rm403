bdc9fcb7510d3ecf4ba966802d34a439fe7bb275
slhaines <shauna@ahsoftware.net>
2012-07-18 15:19:45 -0700

    Fix typo in static_pages_spec; Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,10 +8,14 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-					  :text => "#{base_title} | Home")
+					  :text => "Ruby on Rails Tutorial Sample App")
+    end
+	 it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
   describe "Help page" do
@@ -22,7 +26,7 @@ describe "StaticPages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-					  :text => "{base_title} | Help")
+					  :text => "#{base_title} | Help")
     end
   end
   describe "About page" do
@@ -33,7 +37,7 @@ describe "StaticPages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-					  :text => "{base_title} | About Us")
+					  :text => "#{base_title} | About Us")
     end
   end
   describe "Contact page" do
@@ -44,7 +48,7 @@ describe "StaticPages" do
     it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-					  :text => "{base_title} | Contact Us")
+					  :text => "#{base_title} | Contact Us")
     end
   end
 end
