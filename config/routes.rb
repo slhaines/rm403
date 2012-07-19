0b0501f280a8eb89e2b93019a9c764fd76d7ff62
slhaines <shauna@ahsoftware.net>
2012-07-19 14:07:17 -0700

    Finish layout and routes, including signup_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rm403::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
   match 'help',    to: 'static_pages#help'
   match 'about',   to: 'static_pages#about'
   match 'contact', to: 'static_pages#contact'
+  match 'signup',  to: 'users#new'
 #  get "static_pages/home"
 #  get "static_pages/help"
 #  get "static_pages/about"
