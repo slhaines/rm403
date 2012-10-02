173295ebed635a534c0cce06478e5d2d6a936c20
slhaines <shauna@ahsoftware.net>
2012-10-02 11:14:34 -0700

    after Chap 10 exercises 6, 1, and 2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,16 +22,54 @@ describe "Static pages" do
 
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-        sign_in user
-        visit root_path
-      end
-      it "should render the user's feed" do
-        user.feed.each do |item|
-          page.should have_selector("li##{item.id}", text: item.content)
+      describe "with only one micropost" do
+        before do
+          FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum uno solamente")
+          sign_in user
+          visit root_path
         end
+        it "should render the user's feed" do
+          user.feed.each do |item|
+            page.should have_selector("li##{item.id}", text: item.content)
+          end
+        end
+        it "should render the singular count of the user's single micropost" do
+          page.should have_selector("span", text: "1 micropost")
+        end
+        it "should not pluralize the word micropost" do
+          page.should_not have_selector("span", text: "microposts")
+        end
+      end
+#      before do
+#        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+#        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+#        sign_in user
+#        visit root_path
+#      end
+#      it "should render the user's feed" do
+#        user.feed.each do |item|
+#          page.should have_selector("li##{item.id}", text: item.content)
+#        end
+#      end
+      describe "with multiple microposts should render the user's feed with pagination" do
+#        before(:all) do
+        before do
+          30.times { FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum multiples") } 
+          sign_in user
+          visit root_path
+        end
+#        after(:all)  { User.delete_all }
+        after { User.delete_all }
+        it "should list each micropost" do
+          Micropost.paginate(page: 1).each do |item|
+            page.should have_selector("li##{item.id}", text: item.content)
+          end
+        end
+        it "should render the count of the user's microposts" do
+          page.should have_selector("span", text: "#{user.feed.count} microposts")
+        end
+#        it { should have_selector("div.pagination") }    # fails, but I don't know why
+        it { should have_selector("a", :href => "/?page=2", :content => "2") }
       end
     end
   end
@@ -41,20 +79,20 @@ describe "Static pages" do
 #	 it { should have_selector('h1',    text: 'Help') }
 #	 it { should have_selector('title', text: full_title('Help')) }
     let(:heading)    { 'Help' }
-	 let(:page_title) { 'Help' }
-	 it_should_behave_like "all static pages"
+	  let(:page_title) { 'Help' }
+	  it_should_behave_like "all static pages"
   end
   describe "About page" do
     before { visit about_path }
     let(:heading)    { 'About' }
-	 let(:page_title) { 'About Us' }
-	 it_should_behave_like "all static pages"
+	  let(:page_title) { 'About Us' }
+	  it_should_behave_like "all static pages"
   end
   describe "Contact page" do
     before { visit contact_path }
     let(:heading)    { 'Contact' }
-	 let(:page_title) { 'Contact Us' }
-	 it_should_behave_like "all static pages"
+	  let(:page_title) { 'Contact Us' }
+	  it_should_behave_like "all static pages"
   end
 
   it "should have the right links on the layout" do
