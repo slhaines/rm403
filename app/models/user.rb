976d4b38dad9af8463ebfcfacf2115e30b7d9b12
slhaines <shauna@ahsoftware.net>
2012-10-24 11:26:13 -0700

    before 11.2 web interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+  # note that "source: :follower" is optional (it's the default in this case)
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -19,6 +25,17 @@ class User < ActiveRecord::Base
     # This is preliminary. See "Following users" for the full implementation.
     Micropost.where("user_id = ?", id)
   end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
+
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
